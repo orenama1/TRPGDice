2a692db308d80cb3d7e9cd4b19af815338d39cd0
orenama1 <orenama1@gmail.com>
2015-09-18 02:54:22 +0900

    import unirx create script

---
+++ TRPGDice/Assets/DiceMain.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+using System;
+public class DiceMain : MonoBehaviour {
+    [SerializeField]
+    InputField _Count;
+    [SerializeField]
+    InputField _Num;
+    [SerializeField]
+    Text _Result;
+    [SerializeField]
+    Button _Action;
+    int temp;
+    System.Random _Random = new System.Random();
+    // Use this for initialization
+    void Start () {
+        var count = _Count.OnValueChangeAsObservable()
+            .Where(x=>!string.IsNullOrEmpty(x))
+            .Where(x => int.TryParse(x, out temp))
+            .Select(x => int.Parse(x));
+
+        var num = _Num.OnValueChangeAsObservable()
+            .Where(x =>! string.IsNullOrEmpty(x))
+            .Where(x => int.TryParse(x, out temp))
+            .Select(x => int.Parse(x));
+
+        var anser = num.CombineLatest(count, (n, c) =>
+          {
+              return RandData(c, n);
+          });
+        anser.SubscribeToText(_Result);
+        _Action.OnClickAsObservable()
+            .Subscribe(_ => anser.SubscribeToText(_Result));
+    }
+	string RandData(int count,int num)
+    {
+        int sum = _Random.Next(1, num), tmp;
+        string s = "(" + sum.ToString();
+        for (int i = 1; i < count; i++)
+        {
+            s += "+";
+            tmp = _Random.Next(1, num);
+            s += tmp.ToString();
+            sum += tmp;
+        }
+        s += ")";
+        return sum.ToString() + s;
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
