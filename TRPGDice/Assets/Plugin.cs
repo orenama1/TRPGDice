a5188219a8dc8df8a683777641d225ffd9e169ba
orenama1 <orenama1@gmail.com>
2015-11-17 16:45:01 +0900

    auto roteton

---
+++ TRPGDice/Assets/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System;
+public class plugins
+{
+    [DllImport("native_activity", CharSet = CharSet.Ansi)]
+
+    private static extern bool Test();
+    public bool ISss()
+    {
+        try
+        {
+            return Test();
+        }
+        catch (Exception e)
+        {
+            return true;
+        }
+    }
+}
---
+++ TRPGDice/Assets/Test.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+using UniRx;
+using UniRx.Triggers;
+using System;
+
+public class Test : MonoBehaviour {
+    bool test1 = true;        
+
+    plugins p;
+	// Use this for initialization
+    void Start()
+    {
+        
+        p=new plugins();
+        this.UpdateAsObservable()
+            .Where(_ => !test1)
+            .Subscribe(_ => Application.Quit());
+        this.UpdateAsObservable()
+            .Select(_ =>
+            {
+                test1 = p.ISss();
+                return test1;
+            })
+            .Subscribe(x => {  });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
